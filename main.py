8711f90e17a2211360f0673e9ed1afc6765d942d
Joey Thomas <jothomas@csumb.edu>
2017-08-07 16:40:50 -0700

    importing some jinja stuff

--- main.py
+++ main.py
@@ -15,9 +15,15 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        template = jinja_environment.get_template('templates/main.html')
         self.response.write('Hello world!')
 
 app = webapp2.WSGIApplication([
