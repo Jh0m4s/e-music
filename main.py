f5a22e06c8aaf5e2051e7706fbd448bd166b7e61
Joey Thomas <jothomas@csumb.edu>
2017-08-10 09:40:47 -0700

    Bug fixes

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class Song():
 
   def findscore(self, result):
     dif = []
-    for i in len(result):
+    for i in range(4):
       if abs(result[i] - self.ratings[i] == 0):
         continue
       dif.append(abs(result[i] - self.ratings[i]))
